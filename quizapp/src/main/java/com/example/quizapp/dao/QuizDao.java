947fa5acd376eed70eda11ae8b4cece8406fa7c1
YashwanthSrinivas <srinivasgraj09@gmail.com>
2024-10-24 19:31:55 +0530

    Initial commit for Quiz App

---
+++ quizapp/src/main/java/com/example/quizapp/dao/QuizDao.java
@@ -0,0 +1,10 @@
+package com.example.quizapp.dao;
+
+
+import com.example.quizapp.model.Quiz;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuizDao extends JpaRepository<Quiz, Integer> {
+
+
+}
